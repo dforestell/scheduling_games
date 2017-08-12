be609c6a755476198e73b7a82d6a7ce336c9062e
Dillon Forestell <djlf729@gmail.com>
2017-08-12 08:58:42 -0500

    modify models and migrations, add a seed file

--- db/migrate/20170811204930_create_games.rb
+++ db/migrate/20170811204930_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
   	create_table :games do |t|
-  		t.time :time, null: false
-  		t.date :date, null: false
+  		t.string :time, null: false
+  		t.string :date, null: false
   		t.string :field_address, null: false
   		t.integer :host_id, null: false
   		t.integer :traveler_id
