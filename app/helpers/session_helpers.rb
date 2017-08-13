f1f8a1a4d010feab8dcbd2e540d0b880c96375f9
Dillon Forestell <djlf729@gmail.com>
2017-08-13 16:38:44 -0500

    add ability to register and log in, also add erorrs partial and session helpers

---
+++ app/helpers/session_helpers.rb
@@ -0,0 +1,13 @@
+helpers do
+  def current_user
+    @current_user ||= User.find_by(id: session[:id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate!
+    redirect '/' unless logged_in?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,32 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
   has_many :hosted_games, class_name: :Game, foreign_key: :host_id
   has_many :away_games, class_name: :Game, foreign_key: :traveler_id
+
+
+validates :email, presence: true
+validates :email, uniqueness: true
+validate :validate_password
+
+def validate_password
+  	if @raw_password.nil?
+  		errors.add(:password, "is required")
+  	elsif @raw_password.length < 6
+  		errors.add(:password, "must be 6 characters or more")
+  	end
+  end
+
+  def password
+  	@password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+  	@raw_password = new_password
+  	@password = BCrypt::Password.create(new_password)
+  	self.password_hash = @password
+  end
+
+  def authenticate(password)
+  	self.password == password
+  end
 end
