050931147bbaff2418cf790e9735afb9cdcef347
Dillon Forestell <djlf729@gmail.com>
2017-08-16 13:37:59 -0500

    add availble games link ability to agree to play against a team that has created a hosted game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
-get '/games' do 
+get '/games' do
 	@games = Game.all
 	erb :'games/index'
-end 
+end
 
-get '/games/new' do 
+get '/games/new' do
 	if logged_in?
 		erb :'games/new'
 	else
@@ -11,12 +11,34 @@ get '/games/new' do
 	end
 end
 
-get '/games/:id' do 
+get '/games/available' do
+	if logged_in?
+		@games = Game.where(traveler_id: nil)
+		erb :'games/available'
+	else
+		redirect '/'
+	end
+
+end
+
+get '/games/:id' do
 	@game = Game.find(params[:id])
 	erb :'games/show'
 end
 
-post '/games' do 
+post '/games/:id' do
+	@game = Game.find(params[:id])
+	p @game
+	if logged_in?
+
+		@game.update(traveler_id: current_user.id)
+			redirect "/games/#{@game.id}"
+	else
+		redirect '/'
+	end
+end
+
+post '/games' do
 	@game = Game.new(params[:game])
 	@game.host_id = current_user.id
 
