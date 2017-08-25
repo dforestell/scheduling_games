f003a041220516badb32349cf979e9db113ee93e
Dillon Forestell <djlf729@gmail.com>
2017-08-25 14:40:32 -0500

    add a method on the Game class that formats time the way I want it to be formatted

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,11 @@
 class Game < ActiveRecord::Base
   belongs_to :host, class_name: :User
   belongs_to :traveler, class_name: :User
+
+
+  def format_date
+    str_time = self.date + " " + self.time
+    time = Time.parse(str_time)
+    time.strftime("%A, %b %d %I:%M: %p")
+  end
 end
